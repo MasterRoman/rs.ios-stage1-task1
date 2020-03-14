d1b2f3e66b604ff5d953cd486ef223da37085c3c
MasterRoman <53902992+MasterRoman@users.noreply.github.com>
2020-03-14 23:29:34 +0300

    feat: solve third problem

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,23 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    int billSum = 0;
+    int wrong = 0;
+    int correct = 0;
+    NSString *res = @"";
+    for (int i=0; i<bill.count; i++) {
+        billSum += [bill[i] intValue];
+    }
+    wrong = billSum / 2;
+    correct = billSum - [bill[index] intValue];
+    if ((correct / 2) == [sum intValue]){
+        res = @"Bon Appetit"; } else
+        {
+            wrong = [sum intValue] - correct / 2;
+            res = [NSString stringWithFormat:@"%d", wrong];
+        
+    }
+    return res;
 }
 
 @end
