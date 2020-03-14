a59c44727eacde4128dfd4b62044d05177c8f262
MasterRoman <53902992+MasterRoman@users.noreply.github.com>
2020-03-14 23:26:42 +0300

    feat: solve second problem

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,17 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    NSArray *sortedArray = [array sortedArrayUsingSelector:@selector(compare:)];
+    int sumMin=0;
+    int sumMax=0;
+    for (int i=0; i<sortedArray.count-1; i++) {
+         sumMin += [sortedArray[i] intValue];
+    }
+    sumMax = sumMin - [sortedArray[0] intValue] + [sortedArray[sortedArray.count-1] intValue];
+    NSNumber *min = [NSNumber numberWithInt: sumMin];
+    NSNumber *max = [NSNumber numberWithInt: sumMax];
+    NSArray *solution = @[min,max];
+    return solution;
 }
 
 @end
