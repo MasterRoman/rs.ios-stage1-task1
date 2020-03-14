0c07477a0969536eadeee5a33188351dd296f513
MasterRoman <53902992+MasterRoman@users.noreply.github.com>
2020-03-15 00:42:33 +0300

    fix: bugs

--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -6,6 +6,7 @@
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
     int mistakes = 0;
     int zmist = 0;
+    int zz = [k intValue];
     for (int i=0; i<s.length/2; i++) {
         if ([s characterAtIndex:i]!=[s characterAtIndex:([n intValue]-i-1)]){
             if (([s characterAtIndex:i]==[@'9' charValue] ) || ([s characterAtIndex:([n intValue]-i-1)]==[@'9' charValue])) {
@@ -19,15 +20,16 @@
     if ([k intValue]<zmist){
         return @"-1";
     }
-    mistakes+=zmist/2;
+
     for (int i=0; i<s.length/2; i++) {
         if ([s characterAtIndex:i]!=[s characterAtIndex:([n intValue]-i-1)]){
             char first = [s characterAtIndex:i];
             char second = [s characterAtIndex:[n intValue]-i-1];
-            if (([k intValue]>=2) && (mistakes>=2)  && (first != [@'9' charValue])){
+            if ((zz>=2) && (mistakes>=2)  && (first != [@'9' charValue])){
               s = [s stringByReplacingCharactersInRange:NSMakeRange(i, 1) withString:@"9"];
               s = [s stringByReplacingCharactersInRange:NSMakeRange(s.length-i-1, 1) withString:@"9"];
                 mistakes-=2;
+                zz-=2;
             }
             else if (first>second){
                 s = [s stringByReplacingCharactersInRange:NSMakeRange(s.length-i-1, 1) withString:[NSString stringWithFormat:@"%c",first]];
