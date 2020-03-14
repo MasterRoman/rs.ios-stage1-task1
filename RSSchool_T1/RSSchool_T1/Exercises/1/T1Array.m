a6299be1e1ac8ae6527c75b5e2fdc50c04edac2a
MasterRoman <53902992+MasterRoman@users.noreply.github.com>
2020-03-14 23:25:11 +0300

    feat: solve first problem

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,20 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *mutableArray = [sadArray mutableCopy];
+    if (mutableArray.count>0) {
+        for (int i=1; i<mutableArray.count-1; i++) {
+            
+            int sum = [mutableArray[i-1] intValue] + [mutableArray[i+1] intValue];
+            if ([mutableArray[i] intValue]>sum){
+                [mutableArray removeObjectsAtIndexes:[NSIndexSet indexSetWithIndex:i]];
+                if (i>=2)
+                {i-=2;
+                }
+            }
+        }
+    }
+    return mutableArray ;
 }
 
 @end
